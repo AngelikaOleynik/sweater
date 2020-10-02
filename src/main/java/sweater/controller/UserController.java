9606b5b492b0b914bee2a98ca53e9a48b04168df
Angelika <oleinik38@gmail.com>
2020-10-02 13:09:50 +0400

    migration v1 and v2 добавлен профиль пользователя, смена email

--- src/main/java/sweater/controller/UserController.java
+++ src/main/java/sweater/controller/UserController.java
@@ -2,36 +2,36 @@ package sweater.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import sweater.domain.Role;
 import sweater.domain.User;
-import sweater.repository.UserRepository;
+import sweater.service.UserService;
 
-import java.util.Arrays;
 import java.util.Map;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/user")
-@PreAuthorize("hasAuthority('ADMIN')")  //проверка на админа
+
 public class UserController {
 
-    private UserRepository userRepository;
+    private UserService userService;
     @Autowired
-    public void setUserRepository(UserRepository userRepository) {
-        this.userRepository = userRepository;
+    public void setUserService(UserService userService) {
+        this.userService = userService;
     }
 
+    @PreAuthorize("hasAuthority('ADMIN')")  //проверка на админа
     @GetMapping
     public String userList(Model model) {
-        model.addAttribute("users", userRepository.findAll());
+        model.addAttribute("users", userService.findAll());
 
         return "userList";
     }
 
+    @PreAuthorize("hasAuthority('ADMIN')")  //проверка на админа
     @GetMapping("{user}") //мэппинг, помимо части "/user"  через / будет id
     public String userEditForm(@PathVariable User user, Model model) {
         model.addAttribute("user", user);
@@ -40,28 +40,31 @@ public class UserController {
         return "userEdit";
     }
 
+    @PreAuthorize("hasAuthority('ADMIN')")  //проверка на админа
     @PostMapping
     public String userSave(
             @RequestParam String username,
             @RequestParam Map<String, String> form,
             @RequestParam("userId") User user
     ) {
-        user.setUsername(username);
-
-        Set<String> roles = Arrays.stream(Role.values())
-                .map(Role::name)
-                .collect(Collectors.toSet());
-
-        user.getRoles().clear();
-
-        for (String key : form.keySet()) {
-            if (roles.contains(key)) {
-                user.getRoles().add(Role.valueOf(key));
-            }
-        }
-
-        userRepository.save(user);
-
+        userService.saveUser(user, username, form);
         return "redirect:/user";
     }
+
+    @GetMapping("profile")
+    public String profile(Model model, @AuthenticationPrincipal User user){
+        model.addAttribute("username", user.getUsername());
+        model.addAttribute("email", user.getEmail());
+        return "profile";
+    }
+
+    @PostMapping("profile")
+    public String updateProfile(@AuthenticationPrincipal User user,
+                                @RequestParam String password,
+                                @RequestParam String email){
+        userService.updateProfile(user, password, email);
+        return "redirect:/user/profile";
+
+    }
+
 }
