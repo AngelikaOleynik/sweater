f46c47ffb43e3607ec76d6522243799388d70122
Angelika <oleinik38@gmail.com>
2020-10-02 15:43:10 +0400

    bean validation, шифрование паролей

--- src/main/java/sweater/domain/User.java
+++ src/main/java/sweater/domain/User.java
@@ -23,9 +23,6 @@ public class User implements UserDetails {
     @NotBlank(message = "Password can not be empty")
     private String password;
 
-    //для автоматической проверки паролей
-    @Transient //не добавлять в бд и не пытаться получить это поле из бд
-    private String password2;
     private boolean active;
 
     @Email(message = "Email is not correct") //проверка правильности вводда email
@@ -123,11 +120,4 @@ public class User implements UserDetails {
         this.activationCode = activationCode;
     }
 
-    public String getPassword2() {
-        return password2;
-    }
-
-    public void setPassword2(String password2) {
-        this.password2 = password2;
-    }
 }
